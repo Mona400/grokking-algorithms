b8f7cb9392e1bbeec6022e0a15d41d3882d633db
CSMona <89868260+CSMona@users.noreply.github.com>
2021-09-11 21:43:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3_Part3_recursion
+{
+    class Program
+    {
+        private static int Fact(int item)
+        {
+            if (item <= 1)
+                return 1;
+            return item * Fact(item - 1);
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Fact(5));
+        }
+
+    }
+}
