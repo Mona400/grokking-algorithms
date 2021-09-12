1f90214255df32534c06674e8e009919a2029a8b
CSMona <89868260+CSMona@users.noreply.github.com>
2021-09-12 12:59:28 +0200

    Add files via upload

---
+++ Ch4_Part1_QuickSort_Loop_Sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_Part1_Qui_Loop_Sum
+{
+    class Program
+    {
+        private static int Sum(IEnumerable<int> arr)
+        {
+            var total = 0;
+            foreach (var item in arr)
+            {
+                total += item;
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(new[] { 1, 2, 3, 4 }));
+        }
+    }
+}
