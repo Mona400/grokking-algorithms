7f41ccf071dc18cdfd32b1ab03d1bfd94594c3c5
CSMona <89868260+CSMona@users.noreply.github.com>
2021-09-12 13:28:08 +0200

    Add files via upload

---
+++ Ch4_Part2_QuickSort_RecursiveSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_Part2_QuickSort_RecursiveSum
+{
+    class Program
+    {
+        private static int Sum(IEnumerable<int> list)
+        {
+            if (!list.Any())
+                return 0;
+            return list.Take(1).First() + Sum(list.Skip(1));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(new[] { 1,2,3,4}));
+        }
+    }
+}
