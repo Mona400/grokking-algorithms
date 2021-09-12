207189bd9cce889511f1bdecf04e74be51251860
CSMona <89868260+CSMona@users.noreply.github.com>
2021-09-12 13:34:22 +0200

    Add files via upload

---
+++ Ch4_Part3_QuickSort_RecursiveCount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_Part3_QuickSort_RecursiveCount
+{
+    class Program
+    {
+        private static int Count(IEnumerable<int> list)
+        {
+            if (!list.Any())
+                return 0;
+            return 1 + Count(list.Skip(1));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Count(new[] { 1,2,3,4,5,6}));
+        }
+    }
+}
