0c0bb1bc95bb995ad385cebb95e7702c9981fb4d
CSMona <89868260+CSMona@users.noreply.github.com>
2021-09-18 14:22:12 +0200

    Add files via upload

---
+++ CH5_Part1_HashTable_Price_Of_Groceries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH5_HashTable_Part1_Price_Of_Groceries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Dictionary<string, decimal>();
+            book.Add("apple", 0.67m);
+            book.Add("milk", 1.49m);
+            book.Add("avocado", 1.49m);
+            foreach (var item in book)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
