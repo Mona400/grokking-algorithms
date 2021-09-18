b110a5736fdcf3c7deece4d80911d7588779bbc6
CSMona <89868260+CSMona@users.noreply.github.com>
2021-09-18 15:00:47 +0200

    Add files via upload

---
+++ Ch5_Part2_HashTable_Check_Voter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_Part2_HashTable_Check_Voter
+{
+    class Program
+    {
+       
+        public static Dictionary<string, bool> _voted = new Dictionary<string, bool>();
+        public static void Main(string[] args)
+        {
+
+            _voted.Add("tom", false);
+            _voted.Add("mike", false);
+            _voted.Add("sara", true);
+            _voted.Add("Mona", false);
+
+            CheckVoter("tom");
+            CheckVoter("mike");
+            CheckVoter("sara");
+            CheckVoter("Mohamed");
+        }
+
+        private static void CheckVoter(string name)
+        {
+
+            if (_voted.ContainsKey(name))
+            {
+                Console.WriteLine("kick them out!");
+            }
+            else
+            {
+                _voted.Add(name, true);
+                Console.WriteLine("let them vote!");
+            }
+        }
+    }
+}
